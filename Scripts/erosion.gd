c830141ee1d9688a1a07591adcd92ab4eb76e018
k23000336 <mohammed.s.hossain@kcl.ac.uk>
2025-05-20 16:48:08 +0100

    minor script added

--- Scripts/erosion.gd
+++ Scripts/erosion.gd
@@ -1,9 +1,26 @@
 extends MeshInstance3D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var m = self.mesh
+	
+	var st = SurfaceTool.new()
+	st.create_from(m,0)
+	var arr = st.commit()
+	
+	var tool = MeshDataTool.new()
+	if arr.get_surface_count() > 0:
+		tool.create_from_surface(arr, 0)
+
+		for i in range(tool.get_vertex_count()):
+			var vertex = tool.get_vertex(i)
+			#print("Vertex ", i, ": ", vertex)
+
+		tool.clear()
+	else:
+		print("Mesh has no surfaces.")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
